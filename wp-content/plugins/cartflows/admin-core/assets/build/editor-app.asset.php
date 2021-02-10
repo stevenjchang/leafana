9824af916b23711d32769bba376da964e8ec66ee
stevenjchang <stevenjchang@gmail.com>
2021-02-10 20:56:28 +0000

    init

---
+++ wp-content/plugins/cartflows/admin-core/assets/build/editor-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e17031ef79f27041bb47945ffd7909ff');
---
+++ wp-content/plugins/cartflows/admin-core/assets/build/settings-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '46cfc17c7a53fa1735daf988ef467195');
